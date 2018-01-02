4d47fdd4a4c44348cd45cb38fece259174b0999e
Jose Ramon Roca <jrroca@jrroca.com>
2018-01-02 16:16:41 +0100

    Changed to another command

--- entry.sh
+++ entry.sh
@@ -64,25 +64,60 @@ case "$1" in
     shift # remove argument from array
   fi
 
-  for var in "$@"
-  do
-  	if [ -f $var ]; then
-	  if [ -n "$SSH_PASSWD_SECRET_FILE" ]; then
-		  _command="/ssh-add-pass.sh $var $SSH_PASSWD_SECRET_FILE "
-	  else
-		  _command="ssh-add $var"
-	  fi
-	  debug_msg "Executing: $_command"
+  # Calling ssh-add. This should handle all cases.
+  _command="ssh-add $ssh_key_path $@"
+  debug_msg "Executing: $_command"
 
-	  # When $key_path is empty, ssh-agent will be looking for both id_rsa and id_dsa in the home directory.
-	  # NOTE: We do a sed hack here to strip out '/root/.ssh' from the key path in the output from ssh-add, since this
-	  # path may confuse people.
-	  # echo "Press ENTER or CTRL+C to skip entering passphrase (if any)."
-	  $_command 2>&1 0>&1 | sed 's/\/root\/.ssh\///g'
-	fi
-  done
+  # When $key_path is empty, ssh-agent will be looking for both id_rsa and id_dsa in the home directory.
+  # NOTE: We do a sed hack here to strip out '/root/.ssh' from the key path in the output from ssh-add, since this
+  # path may confuse people.
+  # echo "Press ENTER or CTRL+C to skip entering passphrase (if any)."
+  $_command 2>&1 0>&1 | sed 's/\/root\/.ssh\///g'
 
   # Return first command exit code
+  exit ${PIPESTATUS[0]}
+  ;;
+	ssh-add-pass)
+  shift # remove argument from array
+
+  # .ssh folder from host is expected to be mounted on /.ssh
+  # We copy keys from there into /root/.ssh and fix permissions (necessary on Windows hosts)
+  host_ssh_path="/.ssh"
+  if [ -d $host_ssh_path ]; then
+    debug_msg "Copying host SSH keys and setting proper permissions..."
+    cp -av $host_ssh_path/. ~/.ssh/
+    chmod 700 ~/.ssh
+    chmod 600 ~/.ssh/*
+    chmod 644 ~/.ssh/*.pub
+  fi
+
+  # Make sure the key exists if provided.
+  # When $ssh_key_path is empty, ssh-agent will be looking for both id_rsa and id_dsa in the home directory.
+  ssh_key_path=""
+  if [ -n "$1" ] && [ -f "/root/.ssh/$1" ]; then
+    ssh_key_path="/root/.ssh/$1"
+    shift # remove argument from array
+  fi
+
+  # Calling ssh-add. This should handle all cases.
+  _command="/ssh-add-pass.sh $ssh_key_path $@"
+  debug_msg "Executing: $_command"
+
+  # When $key_path is empty, ssh-agent will be looking for both id_rsa and id_dsa in the home directory.
+  # NOTE: We do a sed hack here to strip out '/root/.ssh' from the key path in the output from ssh-add, since this
+  # path may confuse people.
+  # echo "Press ENTER or CTRL+C to skip entering passphrase (if any)."
+  $_command 2>&1 0>&1 | sed 's/\/root\/.ssh\///g'
+
+  # Return first command exit code
+  exit ${PIPESTATUS[0]}
+  ;;
+	ssh-add-list)
+  shift # remove argument from array
+
+	  _command="ssh-add -l"
+	  
+	  $_command 2>&1 0>&1 | sed 's/\/root\/.ssh\///g'
   exit ${PIPESTATUS[0]}
   ;;
 	*)
